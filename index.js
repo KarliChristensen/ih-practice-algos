ca827c92a5d0975b6b6dfe91b9119bdacbca2567
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-06-02 20:46:06 +0200

    Ekstra - No bonus

--- index.js
+++ index.js
@@ -1,24 +1,93 @@
 // 1. Write a function that takes an array of numbers and returns the largest number in the array.
-function largestNumber(array) {}
+function largestNumber(array) {
+  return Math.max(...array);
+}
+
+console.log(largestNumber([0, 1, 2, 3, 6, 78, 451, 10, 152]));
 
 // 2. Write a function that takes a string and returns the number of vowels in the string.
-function numberOfVowels(string) {}
+const vowels = ["a", "e", "i", "o", "u"];
 
-// 3. Write a function that takes two arrays of numbers and returns a new array with all the elements from both arrays, without any duplicates.
-function combineArrays(array1, array2) {}
+function numberOfVowels(string) {
+  let vowelCount = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (
+      string[i] === "a" ||
+      string[i] === "e" ||
+      string[i] === "i" ||
+      string[i] === "o" ||
+      string[i] === "u"
+    ) {
+      vowelCount++;
+    }
+  }
+  console.log(vowelCount);
+  return vowelCount;
+}
+
+numberOfVowels("Overpopulation");
+
+function combineArrays(array1, array2) {
+  const combinedArray = array1.concat(array2); 
+  const uniqueArray = [...new Set(combinedArray)]; 
+  return uniqueArray;
+}
 
 // 4. Write a function that takes an array of numbers and returns a new array with all the odd numbers in the original array.
-function oddNumbers(array) {}
+function oddNumbers(array) {
+    const oddArray = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 1) {
+        oddArray.push(array[i]);
+      }
+    }
+  
+    return oddArray;
+  }
 
 // 5. Write a function that takes an array of numbers and returns an array with all the even numbers in the original array.
-function evenNumbers(array) {}
+function evenNumbers(array) {
+    const evenArray = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+        evenArray.push(array[i]);
+      }
+    }
+  
+    return evenArray;
+  }
+  
 
 // 6. Write a function that takes two strings and returns a string that contains the characters that appear in both strings.
-function commonCharacters(string1, string2) {}
+function commonCharacters(string1, string2) {
+    let commonChars = '';
+  
+    for (let i = 0; i < string1.length; i++) {
+      const char = string1[i];
+  
+      if (string2.includes(char) && !commonChars.includes(char)) {
+        commonChars += char;
+      }
+    }
+  
+    return commonChars;
+  }
+  
 
 // 7. Write a function that takes an object and returns a new object with all the keys and values reversed (BONUS)
-function reverseObject(object) {}
-
+function reverseObject(object) {
+    const reversedObject = {};
+  
+    for (let key in object) {
+      const value = object[key];
+      reversedObject[value] = key;
+    }
+  
+    return reversedObject;
+  }
+  
 /* Bonuses */
 
 // Palindrome checker: Write a program that takes in a string and returns true if the string is a palindrome (reads the same forwards and backwards).
